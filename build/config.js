1bf2dc84bc3da2e568367ab6675b74e9b5a00c03
Your Name <you@example.com>
2017-09-22 12:34:48 +0530

    remove build forlder

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    "apiUrl": "http://localhost:3001",
-    "secret": "mysecret"
-};
-exports.default = config;
